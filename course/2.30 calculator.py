efb21986ca6a7da7eaecded79884e8f05f501fd7
Schmexus <max.shincko@yandex.ru>
2025-03-10 12:09:48 +0700

    fix calculator
    добавил проверку на корректно действие, так же форматирование под формат децимал для корректной работы с дробями

--- course/2.30 calculator.py
+++ course/2.30 calculator.py
@@ -1,27 +1,42 @@
+from decimal import Decimal
+
 while True:
     primer = input('Введите через пробел число, действие, число: ').split()
     if primer[0] == 'СТОП':
         break
     try:
-        number_1= float(primer[0])
-        number_2=float(primer[2])
+        number_1= Decimal(primer[0])
+        number_2= Decimal(primer[2])
+        if primer[1] not in '+-/*':
+            raise ZeroDivisionError('Это не действие')
+    except ZeroDivisionError:
+        print('Не корректное действие, попробуйте снова')
+        continue
     except:
         print('Вы ввели не числа, либо ввели не через пробел, попробуйте снова')
         continue
     match primer[1]:
         case '+':
             result = number_1 + number_2
+            if result % 1 == 0:
+                result = int(result)
             print(f'{primer[0]} + {primer[2]} = {str(result)}')
         case '-':
             result = number_1-number_2
+            if result % 1 == 0:
+                result = int(result)
             print(f'{primer[0]} - {primer[2]} = {str(result)}')
         case '/':
             if number_2 == 0:
                 print('на 0 делить нельзя. Пробуй снова')
                 continue
             result = number_1/number_2
+            if result % 1 == 0:
+                result = int(result)
             print(f'{primer[0]} / {primer[2]} = {str(result)}')
         case '*':
             result = number_1 * number_2
+            if result % 1 == 0:
+                result = int(result)
             print(f'{primer[0]} * {primer[2]} = {str(result)}')
     print('Введите "СТОП" для выхода')
