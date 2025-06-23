415c0215a2aaee1c78f44937e06aa6f8472358cf
schmex <45226815+schmex@users.noreply.github.com>
2025-06-23 17:25:51 +0700

    Update test_google_maps_api.py

--- course/project/tests/test_google_maps_api.py
+++ course/project/tests/test_google_maps_api.py
@@ -21,13 +21,13 @@ class Test_create_place():
         print(result_get.text)
 
         print('Method PUT')
-        json = { 'address': '100 Lenina street< RU'}
+        json = {'address': '100 Lenina street< RU'}
         result_put = Google_maps_api.change_place(json, place_id)
         print(result_put.text)
 
         print('Method Delete')
         result_delete = Google_maps_api.delete_place(place_id)
-        Check.fields(result_delete,['status           '])
+        Check.fields(result_delete,['status'])
         Check.values_in_fields(result_delete, 'status', 'OK')
 
 
